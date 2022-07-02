a58accaa6b111f5edc816a011f6b58a475dceb5d
osukadesu <64621867+osukadesu@users.noreply.github.com>
2022-07-02 17:51:42 -0500

    Update LogicaImg.cs

--- Assets/MyAssets/Scripts/LogicaImg.cs
+++ Assets/MyAssets/Scripts/LogicaImg.cs
@@ -21,29 +21,58 @@ public class LogicaImg : MonoBehaviour
         {
             notaImg.sprite = Resources.Load<Sprite>("Sprites/DO");
         }
+
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            notaImg.sprite = Resources.Load<Sprite>("Sprites/NEUTRO");
+        }
         if (Input.GetKeyDown(KeyCode.R))
         {
             notaImg.sprite = Resources.Load<Sprite>("Sprites/FA");
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            notaImg.sprite = Resources.Load<Sprite>("Sprites/NEUTRO");
         }
         if (Input.GetKeyDown(KeyCode.Y))
         {
             notaImg.sprite = Resources.Load<Sprite>("Sprites/LA");
         }
+        if (Input.GetKeyUp(KeyCode.Y))
+        {
+            notaImg.sprite = Resources.Load<Sprite>("Sprites/NEUTRO");
+        }
         if (Input.GetKeyDown(KeyCode.E))
         {
             notaImg.sprite = Resources.Load<Sprite>("Sprites/MI");
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            notaImg.sprite = Resources.Load<Sprite>("Sprites/NEUTRO");
         }
         if (Input.GetKeyDown(KeyCode.W))
         {
             notaImg.sprite = Resources.Load<Sprite>("Sprites/RE");
         }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            notaImg.sprite = Resources.Load<Sprite>("Sprites/NEUTRO");
+        }
         if (Input.GetKeyDown(KeyCode.U))
         {
             notaImg.sprite = Resources.Load<Sprite>("Sprites/SI");
         }
+        if (Input.GetKeyUp(KeyCode.U))
+        {
+            notaImg.sprite = Resources.Load<Sprite>("Sprites/NEUTRO");
+        }
         if (Input.GetKeyDown(KeyCode.T))
         {
             notaImg.sprite = Resources.Load<Sprite>("Sprites/SOL");
+        }
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+            notaImg.sprite = Resources.Load<Sprite>("Sprites/NEUTRO");
         }
     }
 }
