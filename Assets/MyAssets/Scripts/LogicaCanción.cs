8c8e6e9969da5425fc0345074e87b464f18242e4
osukadesu <64621867+osukadesu@users.noreply.github.com>
2022-07-05 19:15:02 -0500

    xD

---
+++ Assets/MyAssets/Scripts/LogicaCanción.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaCanción : MonoBehaviour
+{
+     public Transform target;
+     public float speed;
+     void Update() {
+         float step = speed * Time.deltaTime;
+         transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+     }
+}
